e34d0a0fa72fe7f3c4fdaf03a269d19d7113f5c5
PetrashchukIM <petrashchukim@gmail.com>
2024-09-09 13:57:47 +0300

    add export function

--- scripts/ts/card.ts
+++ scripts/ts/card.ts
@@ -1,3 +1,13 @@
+function testF{
+    return 1
+}
+
+
 export default function testF() {
+    console.log("Function from card module");
+}
+
+
+export default function te2() {
     console.log("Function from card module");
 }
